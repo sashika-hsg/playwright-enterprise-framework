0d5a3cb22e3a672d66b6ef5a94c3b3fb6486893b
sashika-hsg <sashika.hsg@gmail.com>
2025-12-22 22:10:07 +0530

    feat(pages): add LoginPage with UI interactions

--- src/pages/loginPage.ts
+++ src/pages/loginPage.ts
@@ -1,8 +1,26 @@
-import { Page } from '@playwright/test';
-import { BasePage } from '@core/basePage';
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "@core/basePage";
 
 export class LoginPage extends BasePage {
+  private readonly usernameInput: Locator;
+  private readonly passwordInput: Locator;
+  private readonly loginButton: Locator;
+
   constructor(page: Page) {
     super(page);
+
+    this.usernameInput = page.locator("#username");
+    this.passwordInput = page.locator("#password");
+    this.loginButton = page.locator('button[type="submit"]');
+  }
+  async enterEmail(email: string): Promise<void> {
+    await this.usernameInput.fill(email);
+  }
+
+  async enterPassword(password: string): Promise<void> {
+    await this.passwordInput.fill(password);
+  }
+  async submitLogin(): Promise<void> {
+    await this.loginButton.click();
   }
 }
